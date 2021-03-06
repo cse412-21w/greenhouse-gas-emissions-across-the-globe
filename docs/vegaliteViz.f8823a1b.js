ffb46b3632e7a7a68a46280529fd167ee5242822
Steven Wilson <stevenw8@uw.edu>
2021-03-06 12:00:52 -0800

    Steven visualization

--- docs/vegaliteViz.f8823a1b.js
+++ docs/vegaliteViz.f8823a1b.js
@@ -117,79 +117,71 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"../static/CO2_GDPdata.csv":[function(require,module,exports) {
+module.exports = "/CO2_GDPdata.7460db92.csv";
+},{}],"vegaliteViz.js":[function(require,module,exports) {
+"use strict";
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+var _CO2_GDPdata = _interopRequireDefault(require("../static/CO2_GDPdata.csv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// import dataset
+"use strict"; // the code should be executed in "strict mode".
+// With strict mode, you can not, for example, use undeclared variables
+
+
+var datapoints = []; // used to store data later
+
+var filtered = []; //var sortedGDP = CO2_GDPdata
+//.filter(d => d['Country'] == 'High income' || d['Country'] == 'Low income' || d['Country'] == 'Upper middle income' || d['Country'] == 'Lower middle income')
+//console.log(CO2_GDPdata)
+
+var options = {
+  config: {// Vega-Lite default configuration
+  },
+  init: function init(view) {
+    // initialize tooltip handler
+    view.tooltip(new vegaTooltip.Handler().call);
+  },
+  view: {
+    // view constructor options
+    // remove the loader if you don't want to default to vega-datasets!
+    //   loader: vega.loader({
+    //     baseURL: "",
+    //   }),
+    renderer: "canvas"
   }
+};
+vl.register(vega, vegaLite, options); // Again, We use d3.csv() to process data
 
-  return bundleURL;
-}
+d3.csv(_CO2_GDPdata.default).then(function (data) {
+  data.forEach(function (d) {
+    datapoints.push(d);
 
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
+    if (d.Country == 'High income' || d.Country == 'Low income' || d.Country == 'Lower middle income' || d.Country == 'Upper middle income') {
+      filtered.push(d);
     }
-  }
+  });
+  drawPlotVegaLite();
+});
 
-  return '/';
+function drawPlotVegaLite() {
+  var selection = vl.selectSingle('Sorted Income').fields('Country', 'Year').init({
+    Country: 'Low income',
+    Year: '1960'
+  }).bind({
+    Country: vl.menu(['Low income', 'High income', 'Upper middle income', 'Lower middle income']),
+    Year: vl.slider(1960, 2016, 1)
+  }); // Nothing changed below
+
+  return vl.markPoint({
+    filled: true
+  }).data(filtered).select(selection).encode(vl.color().if(selection, vl.value('gold')).value("gray"), vl.x().fieldQ('GDP'), vl.y().fieldQ('CO2'), vl.opacity().if(selection).value(0.1), vl.tooltip().fieldQ('Year')).width(450).height(450).render().then(function (viewElement) {
+    document.getElementById('view').appendChild(viewElement);
+  });
 }
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"style.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"../static/CO2_GDPdata.csv":"../static/CO2_GDPdata.csv"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -393,5 +385,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/style.e308ff8e.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","vegaliteViz.js"], null)
+//# sourceMappingURL=/vegaliteViz.f8823a1b.js.map
