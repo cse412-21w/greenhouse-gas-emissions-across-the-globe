d37abd3244923d2224c683917c9c90dd45669c4e
chengj29 <chengj29@uw.edu>
2021-03-09 18:56:40 -0800

    website done

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+document.getElementById("start").addEventListener("click", (event) => {
+  document.getElementById("index").innerHTML = "false";
+  window.location.href="gdp.html";
+  return false;
+});
--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("start").addEventListener("click", (event) => {
-  document.getElementById("index").innerHTML = "";
-  window.location.href="globaltemp.html",true;;
-});
